a09d1697cd3611a6839d3f156a020a84dacfd5b3
Alvaro Salvino <alvaro.salvino@gmail.com>
2023-11-01 21:51:12 -0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,8 +1,11 @@
 from flask import Flask
+from flask_pydantic_sped import FlaskPydanticSpec
 
 server = Flask(__name__)
+spec = FlaskPydanticSpec('flask', title='Live de Python')
+spec.register(server)
 
-@server.get('/pessoas')
+@server.get('/pessoas') # Rota, endpoint, recurso...
 def buscar_pessoas():
     return 'Programaticamente Falando, Vitor Gusmao'
 
