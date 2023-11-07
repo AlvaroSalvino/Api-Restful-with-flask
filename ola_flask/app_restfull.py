4088f06af89b1319e59e99dfb80b2773296e9000
Alvaro Salvino <alvaro.salvino@gmail.com>
2023-11-06 23:51:30 -0300

    Update app_restfull.py

--- ola_flask/app_restfull.py
+++ ola_flask/app_restfull.py
@@ -1,2 +1,44 @@
-from flask import Flask
+from flask import Flask, request
+from flask_restful import Resource, Api
+import json
 
+app = Flask(__name__)
+api = Api(app)
+
+desenvolvedores = [
+    {
+        'id':'0',
+        'nome':'Alvaro',
+         'habilidades':['Python','Flask']
+    },
+
+    {
+        'id':'1',
+        'nome':'Meyre',
+         'habilidades':['Python','Django']
+    }
+]
+class Desenvolvedor(Resource):
+    def get(self, id):
+        try:
+            response = desenvolvedores[id]
+        except IndexError:
+            mensagem = 'Desenvolvedor de ID {} não existe'.format(id)
+            response = {'status': 'erro', 'mensagem': mensagem}
+        except Exception:
+            mensagem = 'Erro desconhecido, Procure o administrador da API'
+            response = {'status': 'erro', 'mensagem': mensagem}
+        return response
+    def put(self, id):
+        dados = json.loads(request.data)
+        desenvolvedores[id] = dados
+        return dados
+    def delete(self):
+        dados = json.loads(request.data)
+        return {'nome':'Alvaro'}
+
+api.add_resource(Desenvolvedor, '/dev/<int:id>')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
