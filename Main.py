564b8d903d8d06e6b89522b041882469eba892ae
Alvaro Salvino <alvaro.salvino@gmail.com>
2023-11-08 21:59:16 -0300

    .

--- Main.py
+++ Main.py
@@ -2,13 +2,13 @@ from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 from sqlalchemy.orm import scoped_session, sessionmaker, relationship
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine('sqlite:///atividades.db', convert_unicode=True)
+engine = create_engine('sqlite:///atividades.db')
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          bind=engine))
-Base = declarative_base()
-Base.query = db_session.query_property()
+base = declarative_base()
+base.query = db_session.query_property()
 
-class Pessoas(Base):
+class Pessoas(base):
     __tablename__='pessoas'
     id = Column(Integer, primary_key=True)
     nome = Column(String(40), index=True)
@@ -25,7 +25,7 @@ class Pessoas(Base):
         db_session.delete(self)
         db_session.commit()
 
-class Atividades(Base):
+class Atividades(base):
     __tablename__='atividades'
     id = Column(Integer, primary_key=True)
     nome = Column(String(80))
@@ -43,7 +43,7 @@ class Atividades(Base):
         db_session.delete(self)
         db_session.commit()
 
-class Usuarios(Base):
+class Usuarios(base):
     __tablename__='usuarios'
     id = Column(Integer, primary_key=True)
     login = Column(String(20), unique=True)
@@ -61,7 +61,7 @@ class Usuarios(Base):
         db_session.commit()
 
 def init_db():
-    Base.metadata.create_all(bind=engine)
+    base.metadata.create_all(bind=engine)
 
 if __name__ == '__main__':
     init_db()
