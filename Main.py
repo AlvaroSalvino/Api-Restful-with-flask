4a2cb58d83d2a33757b733014f36d8ef546d2687
Alvaro Salvino <alvaro.salvino@gmail.com>
2023-11-08 21:56:46 -0300

    .

--- Main.py
+++ Main.py
@@ -1,12 +1,67 @@
-from flask import Flask
-from flask_pydantic_sped import FlaskPydanticSpec
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy.orm import scoped_session, sessionmaker, relationship
+from sqlalchemy.ext.declarative import declarative_base
 
-server = Flask(__name__)
-spec = FlaskPydanticSpec('flask', title='Live de Python')
-spec.register(server)
+engine = create_engine('sqlite:///atividades.db', convert_unicode=True)
+db_session = scoped_session(sessionmaker(autocommit=False,
+                                         bind=engine))
+Base = declarative_base()
+Base.query = db_session.query_property()
 
-@server.get('/pessoas') # Rota, endpoint, recurso...
-def buscar_pessoas():
-    return 'Programaticamente Falando, Vitor Gusmao'
+class Pessoas(Base):
+    __tablename__='pessoas'
+    id = Column(Integer, primary_key=True)
+    nome = Column(String(40), index=True)
+    idade = Column(Integer)
 
-server.run()
+    def __repr__(self):
+        return '<Pessoa {}>'.format(self.nome)
+
+    def save(self):
+        db_session.add(self)
+        db_session.commit()
+
+    def delete(self):
+        db_session.delete(self)
+        db_session.commit()
+
+class Atividades(Base):
+    __tablename__='atividades'
+    id = Column(Integer, primary_key=True)
+    nome = Column(String(80))
+    pessoa_id = Column(Integer, ForeignKey('pessoas.id'))
+    pessoa = relationship("Pessoas")
+
+    def __repr__(self):
+        return '<Atividades {}>'.format(self.nome)
+
+    def save(self):
+        db_session.add(self)
+        db_session.commit()
+
+    def delete(self):
+        db_session.delete(self)
+        db_session.commit()
+
+class Usuarios(Base):
+    __tablename__='usuarios'
+    id = Column(Integer, primary_key=True)
+    login = Column(String(20), unique=True)
+    senha = Column(String(20))
+
+    def __repr__(self):
+        return '<Usuario {}>'.format(self.login)
+
+    def save(self):
+        db_session.add(self)
+        db_session.commit()
+
+    def delete(self):
+        db_session.delete(self)
+        db_session.commit()
+
+def init_db():
+    Base.metadata.create_all(bind=engine)
+
+if __name__ == '__main__':
+    init_db()
